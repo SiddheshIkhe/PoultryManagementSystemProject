b88478ac7ea621878a44c73c8528c5d0edea56c5
SiddheshIkhe <160866928+SiddheshIkhe@users.noreply.github.com>
2026-04-15 20:19:13 +0530

    security config update for cross origin error

--- poultryManagementSystem/src/main/java/com/example/poultryManagementSystem/configuration/SecurityConfig.java
+++ poultryManagementSystem/src/main/java/com/example/poultryManagementSystem/configuration/SecurityConfig.java
@@ -29,17 +29,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                            .allowedMethods("*")
-                            .allowedHeaders("*");
-                }
-            };
-        }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 
 }
